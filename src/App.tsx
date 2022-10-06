1a8fd6122432ebba26d1d0d078acd9fdfe3fab80
Anton Gunnarsson <gson.anton@hotmail.com>
2022-10-06 23:29:08 +0200

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ChildCount from './demos/childcount/ChildCount';
 import FormError from './demos/error/FormError';
 import Hover from './demos/hover/Hover';
@@ -5,14 +6,31 @@ import Other from './demos/other/Other';
 import Tabs from './demos/tabs/Tabs';
 
 export default function App() {
+  const [stage, setStage] = useState(0);
+
+  const parts = [Intro, Hover, Tabs, Other, ChildCount, FormError, Thanks];
+
+  const Current = parts[stage];
+
   return (
     <div className="mx-auto flex h-full items-center justify-center p-4 text-center">
-      {/* <Intro /> */}
-      {/* <Hover /> */}
-      {/* <Tabs /> */}
-      {/* <Other /> */}
-      {/* <ChildCount /> */}
-      <FormError />
+      {stage < parts.length - 1 && (
+        <button
+          className="absolute top-2 right-2 mx-auto mt-auto w-fit rounded border-b-4 border-gray-700 bg-gray-500 py-2 px-4 font-bold text-white hover:border-gray-500 hover:bg-gray-400"
+          onClick={() => setStage(stage + 1)}
+        >
+          Next
+        </button>
+      )}
+      {stage > 0 && (
+        <button
+          className="absolute top-2 left-2 mx-auto mt-auto w-fit rounded border-b-4 border-gray-700 bg-gray-500 py-2 px-4 font-bold text-white hover:border-gray-500 hover:bg-gray-400"
+          onClick={() => setStage(stage - 1)}
+        >
+          Prev
+        </button>
+      )}
+      <Current />
     </div>
   );
 }
@@ -26,6 +44,17 @@ function Intro() {
       <p className="mt-24 mb-8 text-6xl">Anton Gunnarsson</p>
       <p className="my-8 text-4xl">Mpya Digital</p>
       <p className="my-8 text-4xl">www.asdf.pizza</p>
+    </div>
+  );
+}
+
+function Thanks() {
+  return (
+    <div>
+      <h1 className="text-8xl">Thank you!</h1>
+      <p className="mt-24 mb-8 text-6xl">Anton Gunnarsson</p>
+      <p className="my-8 text-4xl">Mpya Digital</p>
+      <p className="my-8 text-4xl">@awnton</p>
     </div>
   );
 }
